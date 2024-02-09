afe25a283a574aade0646ebb8a289d98ee622385
fangyeming <fangyemingzz@gmail.com>
2024-02-09 15:23:28 +0800

    Update MOSSAI_NFT_Product deployment and setContractAddress

--- scripts/MOSSAI_NFT_Product.ts
+++ scripts/MOSSAI_NFT_Product.ts
@@ -6,13 +6,13 @@ async function main() {
 
 
 
-    const MOSSAI_Storage = await ethers.deployContract("MOSSAI_Storage");
+    const MOSSAI_Storage = await ethers.deployContract("MOSSAI_Storage", [process.env.ADMIN_Wallet_Address]);
     await MOSSAI_Storage.waitForDeployment()
 
 
 
     const contract = await ethers.getContractFactory("MOSSAI_NFT_Product");
-    const instance = await upgrades.deployProxy(contract);
+    const instance = await upgrades.deployProxy(contract, [process.env.ADMIN_Wallet_Address]);
     await instance.waitForDeployment();
 
 
@@ -20,12 +20,12 @@ async function main() {
 
 
 
-    await (await instance.setContractAddress([
-        "0x0000000000000000000000000000000000000000",
-        '0x9bDaf3912e7b4794fE8aF2E748C35898265D5615',
-        '0xD11F65E5A55Cd7CA459a659734951901c8E57D30',
-        MOSSAI_Storage.target
-    ])).wait()
+    // await (await instance.setContractAddress([
+    //     "0x0000000000000000000000000000000000000000",
+    //     '0x9bDaf3912e7b4794fE8aF2E748C35898265D5615',
+    //     '0xD11F65E5A55Cd7CA459a659734951901c8E57D30',
+    //     MOSSAI_Storage.target
+    // ])).wait()
 
 
     // const MOSSAI_NFT_Market = await ethers.getContractAt("MOSSAI_NFT_Market", "0xa52F3f2d571D069B6A3ce0297Ee9438B21400CDf");
